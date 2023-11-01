53759ce59126895064f6c25ab667da6710cfdba8
113297903+Geeh17@users.noreply.github.com <141880337+GldaSilva17@users.noreply.github.com>
2023-10-31 23:37:46 -0300

    Modelo de dominio - Entity Framework Core

---
+++ LanchesMac/LanchesMac/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public int Descricao { get; set; }
+
+    }
+}
---
+++ LanchesMac/LanchesMac/Models/Lanche.cs
@@ -0,0 +1,16 @@
+﻿namespace LanchesMac.Models
+{
+    public class Lanches
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public string Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public string IsLanchePreferido { get; set; }
+        public string EmEstoque { get; set; }
+    
+    }
+}
