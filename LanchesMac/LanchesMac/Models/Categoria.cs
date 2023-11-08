e0036ae6bd564cf31fe0805d9119d479515d2ed9
113297903+Geeh17@users.noreply.github.com <141880337+GldaSilva17@users.noreply.github.com>
2023-11-08 19:25:36 -0300

    Configurando EF Core

--- LanchesMac/LanchesMac/Models/Categoria.cs
+++ LanchesMac/LanchesMac/Models/Categoria.cs
@@ -4,7 +4,8 @@
     {
         public int CategoriaId { get; set; }
         public string CategoriaNome { get; set; }
-        public int Descricao { get; set; }
+        public string Descricao { get; set; }
 
+        public List<Lanche> Lanches { get; set; }
     }
 }
